fc03c6e118d337e13532d9e9752fcdc6a0d3a5b3
Alejandro Alvarez <75654769+ElPatronHH@users.noreply.github.com>
2023-12-07 16:00:01 -0600

    conexión a db local

--- database/database.py
+++ database/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # Configura la cadena de conexión para la base de datos de Azure
 # Reemplaza 'topicos' con tu nombre de usuario y 'Admin12345' con tu contraseña
-URL_DATABASE = 'mysql+mysqlconnector://topicos:Admin12345@iproconnectdb.mysql.database.azure.com:3306/iproconnectmaterials'
+URL_DATABASE = 'mysql+mysqlconnector://root@localhost/iproconnectmaterials'
 
 engine = create_engine(URL_DATABASE)
 
