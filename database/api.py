0887b55964e6c89a1d39a5c38993e3cbbf559aa2
Alejandro Alvarez <75654769+ElPatronHH@users.noreply.github.com>
2023-12-07 15:50:33 -0600

    ips at localhost

--- database/api.py
+++ database/api.py
@@ -2,35 +2,35 @@ import httpx
 
 async def getStock():
     async with httpx.AsyncClient() as client:
-        response = await client.get("http://40.76.247.213:8000/backend/stockfull")
+        response = await client.get("http://localhost:8000/backend/stockfull")
     if response.status_code == 200:
         result = response.json()
         return result
 
 async def getPedidosEntrantes():
     async with httpx.AsyncClient() as client:
-        response = await client.get("http://40.76.247.213:8000/backend/pedidosEntrantes")
+        response = await client.get("http://localhost:8000/backend/pedidosEntrantes")
     if response.status_code == 200:
         result = response.json()
         return result
     
 async def getHistorialDePedidos():
     async with httpx.AsyncClient() as client:
-        response = await client.get("http://40.76.247.213:8000/backend/historialDePedidos")
+        response = await client.get("http://localhost:8000/backend/historialDePedidos")
     if response.status_code == 200:
         result = response.json()
         return result
     
 async def getDePedidosEnCurso():
     async with httpx.AsyncClient() as client:
-        response = await client.get("http://40.76.247.213:8000/backend/pedidosEnCurso")
+        response = await client.get("http://localhost:8000/backend/pedidosEnCurso")
     if response.status_code == 200:
         result = response.json()
         return result
     
 async def deleteProducto(stock_id):
     async with httpx.AsyncClient() as client:
-        response = await client.delete(f"http://40.76.247.213:8000/stock/{stock_id}")
+        response = await client.delete(f"http://localhost:8000/stock/{stock_id}")
     if response.status_code == 200:
         return True  
     else:
@@ -38,7 +38,7 @@ async def deleteProducto(stock_id):
     
 async def deleteProductoLogico(producto_id):
     async with httpx.AsyncClient() as client:
-        response = await client.put(f"http://40.76.247.213:8000/backend/productos/{producto_id}/cambiar-status?new_status=0")
+        response = await client.put(f"http://localhost:8000/backend/productos/{producto_id}/cambiar-status?new_status=0")
     if response.status_code == 200:
         return True  
     else:
@@ -46,7 +46,7 @@ async def deleteProductoLogico(producto_id):
     
 async def modifyProducto(producto_id, update_data):
     async with httpx.AsyncClient() as client:
-        url = f"http://40.76.247.213:8000/backend/productos/{producto_id}"
+        url = f"http://localhost:8000/backend/productos/{producto_id}"
         response = await client.put(url, json=update_data) 
     if response.status_code == 200:
         return True
@@ -54,14 +54,14 @@ async def modifyProducto(producto_id, update_data):
         return False
 
 async def addProducto(producto_data):
-    url = "http://40.76.247.213:8000/backend/addproduct" 
+    url = "http://localhost:8000/backend/addproduct" 
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=producto_data)
     if response.status_code == 201:
         return response.json()
     
 async def rechazarPedido(pedido_id,update_data):
-    url = f"http://40.76.247.213:8000/backend/pedidos/{pedido_id}" 
+    url = f"http://localhost:8000/backend/pedidos/{pedido_id}" 
     async with httpx.AsyncClient() as client:
         response = await client.put(url, json=update_data)
     if response.status_code == 200:
@@ -70,7 +70,7 @@ async def rechazarPedido(pedido_id,update_data):
         return False
     
 async def aceptarPedido(pedido_id,update_data):
-    url = f"http://40.76.247.213:8000/backend/acceptpedido/{pedido_id}" 
+    url = f"http://localhost:8000/backend/acceptpedido/{pedido_id}" 
     async with httpx.AsyncClient() as client:
         response = await client.put(url, json=update_data)
     if response.status_code == 200:
@@ -81,7 +81,7 @@ async def aceptarPedido(pedido_id,update_data):
 import httpx
 
 async def comprobarStockSuficiente(id_productos):
-    url = "http://40.76.247.213:8000/backend/comprobacion_stock"
+    url = "http://localhost:8000/backend/comprobacion_stock"
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=id_productos)
     if response.status_code == 200:
@@ -90,7 +90,7 @@ async def comprobarStockSuficiente(id_productos):
         return False
     
 async def obtenerDetalleDePedidos(id_pedido):
-    url = f"http://40.76.247.213:8000/backend/detalle_pedidos?pedido_id={id_pedido}"
+    url = f"http://localhost:8000/backend/detalle_pedidos?pedido_id={id_pedido}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
     if response.status_code == 200:
@@ -100,7 +100,7 @@ async def obtenerDetalleDePedidos(id_pedido):
 
 async def modifyStock(producto_id, update_data):
     async with httpx.AsyncClient() as client:
-        url = f"http://40.76.247.213:8000/backend/productos_new_stock/{producto_id}"
+        url = f"http://localhost:8000/backend/productos_new_stock/{producto_id}"
         response = await client.put(url, json=update_data) 
     if response.status_code == 200:
         return response.json()
@@ -108,7 +108,7 @@ async def modifyStock(producto_id, update_data):
         return False
     
 async def getItemFabricacion(producto_id):
-    url = f"http://40.76.247.213:8000/backend/producto_tiempo/{producto_id}"
+    url = f"http://localhost:8000/backend/producto_tiempo/{producto_id}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
     if response.status_code == 200:
@@ -117,7 +117,7 @@ async def getItemFabricacion(producto_id):
         return {"error": f"Error al obtener datos para el producto {producto_id}"}
 
 async def getCompras():
-    url = f"http://40.76.247.213:8000/backend/gethistorialCompras"
+    url = f"http://localhost:8000/backend/gethistorialCompras"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
     if response.status_code == 200:
@@ -126,7 +126,7 @@ async def getCompras():
         return {"Error al obtener datos."}
     
 async def addCompra(compra_data):
-    url = "http://40.76.247.213:8000/backend/compras_logs" 
+    url = "http://localhost:8000/backend/compras_logs" 
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=compra_data)
     if response.status_code == 201:
